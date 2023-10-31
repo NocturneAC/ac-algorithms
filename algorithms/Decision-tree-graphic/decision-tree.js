2f84b2b378809919cac53596da5ceeb4e51b8aac
Nocturne <Nocturne@AnnieCannons.com>
2023-10-31 13:49:00 -0700

    pseudocode completed

--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,3 +1,15 @@
 function transportationOptions() {
     // Write code here to make the decision tree into an algorithm!
+    // what's the weather? input button
+    // weather? overall grandparent
+      // if weather === rain, return bus
+      // else if weather === cloudy, return text input
+        // is hungry? radio select (true/false)
+          // is hungry == true, return walk
+          // is hungry == false, return bus
+      // else if weather === sunny, return text input
+        // how much time, >30 mins or <30mins? radio select (t/f)
+          // time == >30 mins, return walk
+          // time == <30 mins, return bus
+
   }
