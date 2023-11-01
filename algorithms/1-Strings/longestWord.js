a52d43238cc1a99a7366bf91a0fe57e9b51c834f
Nocturne <Nocturne@AnnieCannons.com>
2023-11-01 14:13:38 -0700

    this is a commit message

--- algorithms/1-Strings/longestWord.js
+++ algorithms/1-Strings/longestWord.js
@@ -8,15 +8,41 @@ Read through the function and write comments to explain what is happening in the
  */
 
 function findLongestWord(str) {
+  // the function we are defining called findLongestWord has a parameter we are passing in, called str
   var words = str.split(' ');
+  // defining the variable words with the value of str.split(" ")
+  // this passes in the parameter assigned above on line 10 and assigns a function with it to split an array of substrings, then returns the new array without changing the original string
   var longestWordLength = 0;
+  // this sets the variable longestWordLength to the value of num zero
+  var shortestWordLength = 0;
   
   for (var i = 0; i < words.length; i++) {
+    // this is a for loop with an index indicator of zero, this will count up from zero to determine the length of the variable words
     if (words[i].length > longestWordLength) {
+      // conditional statement: if words[i].length is greater than 0, then count up
       longestWordLength = words[i].length;
+    } else if (words[i].length > shortestWordLength) {
+      shortestWordLength = words[i].length;
     }
   }
   
   return longestWordLength;
+  // this returns the length of the word, stops the function
 }
 
+console.log(words);
+// console.log(str);
+
+// this was another student's solution:
+// function findShortestWord(str) {
+//   var words = str.split(' ');
+//   var shortestWordLength = Infinity;
+  
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[i].length < shortestWordLength) {
+//       shortestWordLength = words[i].length;
+//     }
+//   }
+  
+//   return shortestWordLength;
+// }
